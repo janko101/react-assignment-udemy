93d10e2456b55abb3f4f87e54aaa98c4861fe1e2
Janko <mailodjanka@gmail.com>
2020-05-07 11:02:56 +0200

    Adds a userName state and passes it in a userOutput Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
-import UserInput from './Components/UserInput'
-import UserOutput from './Components/UserOutput'
+import UserInput from "./Components/UserInput";
+import UserOutput from "./Components/UserOutput";
 
 class App extends Component {
+  state = {
+    userNames: [{ userName: "Mad Farmer" }, { userName: "Felga" }],
+  };
   render() {
     return (
       <div className="App">
         <h2>1st Assignment</h2>
-        <UserOutput userName="Mad Farmer"/>
-        <UserOutput userName="Felga" />
+        <UserOutput userName={this.state.userNames[0].userName} />
+        <UserOutput userName={this.state.userNames[1].userName} />
         <UserInput />
       </div>
     );
