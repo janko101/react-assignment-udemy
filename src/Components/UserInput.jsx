cdb2dd26b059205450923c6e7bef7f21b3e48bfd
Janko <mailodjanka@gmail.com>
2020-05-07 11:50:36 +0200

    Adds insline styling to the input field

--- src/Components/UserInput.jsx
+++ src/Components/UserInput.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 const UserInput = (props) => {
+  const inputStyle = {
+    border: "2px solid purple",
+    textAlign: "center",
+    boxShadow: "0px 2px 20px purple",
+    padding: "10px"
+  }
   return (
     <div>
-      <input type="text" onChange={props.changed} value={props.userName}/>
+      <input type="text" style={inputStyle} onChange={props.changed} value={props.userName}/>
     </div>
   );
 };
