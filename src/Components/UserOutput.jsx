9d1aa8ad746e9506e574db88687da3c4f4ee1416
Janko <mailodjanka@gmail.com>
2020-05-07 10:52:32 +0200

    Adds two new components UserInput and UserOutput

---
+++ src/Components/UserOutput.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserOutput = () => {
+  return (
+    <div>
+      <p>1st Paragraph</p>
+      <p>2nd Paragraph</p>
+    </div>
+  );
+};
+
+export default UserOutput;
